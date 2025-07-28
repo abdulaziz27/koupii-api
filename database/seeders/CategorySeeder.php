77e593db493bdb0750a111d98a16338beaedf9fd
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-07-28 21:16:04 +0700

    chore: add CategorySeeder for initial vocabulary categories

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\VocabularyCategory as Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::updateOrCreate([
+            'name' => 'Noun',
+            'color_code' => '#FFC107'
+        ]);
+
+        Category::updateOrCreate([
+            'name' => 'Adjective',
+            'color_code' => '#EE47FF;
+'
+        ]);
+
+        Category::updateOrCreate([
+            'name' => 'Adverb',
+            'color_code' => '#0F68DC'
+        ]);
+
+        Category::updateOrCreate([
+            'name' => 'Verb',
+            'color_code' => '#50BFA5'
+        ]);
+    }
+}

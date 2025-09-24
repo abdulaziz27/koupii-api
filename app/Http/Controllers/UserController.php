535a677fb6d498a1e491796439df10b44041741f
abdulaziz27 <doelziez212@gmail.com>
2025-09-24 22:32:22 +0700

    cleanup: Remove debug logs from UserController
    - Remove debug logging from profile update method
    - Clean up code for production deployment
    - Keep only essential functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -217,42 +217,14 @@ class UserController extends Controller
 
             $data = $validator->validated();
 
-            // Debug request
-            \Log::info('Request data', [
-                'has_file' => $request->hasFile('avatar'),
-                'all' => $request->all(),
-                'files' => $request->allFiles(),
-                'validated' => $data
-            ]);
-            
             if ($request->hasFile('avatar')) {
                 if ($user->avatar) {
                     FileUploadHelper::delete($user->avatar);
                 }
                 $data['avatar'] = FileUploadHelper::upload($request->file('avatar'), 'avatar');
-                
-                // Debug log
-                \Log::info('Avatar upload', [
-                    'file' => $request->file('avatar')->getClientOriginalName(),
-                    'size' => $request->file('avatar')->getSize(),
-                    'path' => $data['avatar']
-                ]);
             }
 
-            // Debug log before update
-            \Log::info('Before update', [
-                'user_id' => $user->id,
-                'avatar_before' => $user->avatar,
-                'data' => $data
-            ]);
-
             $user->update($data);
-
-            // Debug log after update
-            \Log::info('After update', [
-                'user_id' => $user->id,
-                'avatar_after' => $user->avatar
-            ]);
 
             DB::commit();
             $user->refresh();
